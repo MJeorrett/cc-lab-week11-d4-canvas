cd105c7fffe99b6fa9a713a4786ec77f252eaab5
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-08 15:43:37 +0000

    basic painting done

--- public/index.js
+++ public/index.js
@@ -1,18 +1,41 @@
-var drawCircle = function( context, x, y, radius ) {
-  context.moveTo( x + radius, y );
-  context.arc(x, y, radius, degToRad( 0 ), degToRad( 360 ) );
-  context.stroke();
-}
+(function() {
 
-var degToRad = function( degrees ) {
-  return ( degrees * Math.PI ) / 180;
-}
+  var drawing = false;
 
-var app = function() {
-  var canvas = document.querySelector( '#main-canvas' );
-  console.log( "canvas:", canvas );
-  var context = canvas.getContext( '2d' );
-  console.log( "context:", context );
-}
+  var drawCircle = function( context, x, y, radius ) {
+    context.moveTo( x + radius, y );
+    context.arc(x, y, radius, degToRad( 0 ), degToRad( 360 ) );
+    context.stroke();
+  };
 
-window.onload = app;
+  var degToRad = function( degrees ) {
+    return ( degrees * Math.PI ) / 180;
+  };
+
+  var app = function() {
+    var canvas = document.querySelector( '#main-canvas' );
+    console.log( "canvas:", canvas );
+    var context = canvas.getContext( '2d' );
+    console.log( "context:", context );
+
+    canvas.onmousedown = function( ev ) {
+      console.log( "onmousedown" );
+      drawing = true;
+    };
+
+    canvas.onmouseup = function( ev ) {
+      console.log( "mouseup" );
+      drawing = false;
+    };
+
+    canvas.onmousemove = function( ev ) {
+      // console.log( "mousemove" );
+      if ( drawing ) {
+        drawCircle( context, ev.layerX, ev.layerY, 100 );
+      }
+    };
+  };
+
+  window.onload = app;
+
+})();
